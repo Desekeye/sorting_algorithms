7e5568ca0f0048c2547c710856b4f0bf52da6edc
Desekeye <106808436+Desekeye@users.noreply.github.com>
2022-11-09 13:20:24 +0100

    Update deck.h

--- deck.h
+++ deck.h
@@ -1,12 +1,21 @@
 #ifndef DECK_H
 #define DECK_H
 
+#include <stdlib.h>
+
+/**
+ * enum kind_e - Enumeration of card suits.
+ * @SPADE: Spades suit.
+ * @HEART: Hearts suit.
+ * @CLUB: Clubs suit.
+ * @DIAMOND: Diamonds suit.
+ */
 typedef enum kind_e
 {
- SPADE = 0,
- HEART,
- CLUB,
- DIAMOND
+	SPADE = 0,
+	HEART,
+	CLUB,
+	DIAMOND
 } kind_t;
 
 /**
@@ -20,7 +29,6 @@ typedef struct card_s
 {
 	const char *value;
 	const kind_t kind;
-
 } card_t;
 
 /**
@@ -36,4 +44,7 @@ typedef struct deck_node_s
 	struct deck_node_s *prev;
 	struct deck_node_s *next;
 } deck_node_t;
+
+void sort_deck(deck_node_t **deck);
+
 #endif
